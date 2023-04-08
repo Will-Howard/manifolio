e5a288bca592316512e87442c08705052f0f9c17
Will Howard <w.howard256+github@gmail.com>
2023-04-08 21:26:43 +0100

    some testing stuff

--- manifolio-ui/jest.config.js
+++ manifolio-ui/jest.config.js
@@ -1,12 +1,16 @@
-// jest.config.js
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
-  roots: ['<rootDir>'],
+  roots: ['./'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['@swc/jest'],
   },
   moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
--- manifolio-ui/lib/__tests__/calculate.test.ts
+++ manifolio-ui/lib/__tests__/calculate.test.ts
@@ -1,5 +1,17 @@
-describe("ExampleTests", () => {
-  test("noop", () => {
-    expect(1).toBe(1);
-  });
+import { calculateNaiveKellyBet } from "@/lib/calculate";
+
+test("Naive Kelly formula gives expected result", () => {
+  expect(
+    calculateNaiveKellyBet({
+      marketProb: 0.5,
+      estimatedProb: 0.6,
+      deferenceFactor: 0.5,
+      bankroll: 100,
+    })
+  ).toMatchInlineSnapshot(`
+    {
+      "amount": 9.999999999999998,
+      "outcome": "YES",
+    }
+  `);
 });
