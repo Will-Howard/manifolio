b79dcc95f33a9e6f9b62a38a886eba36708f2200
Will Howard <w.howard256+github@gmail.com>
2023-08-05 16:33:06 +0100

    comment out api tests for now

--- manifolio-ui/lib/__tests__/market.test.ts
+++ manifolio-ui/lib/__tests__/market.test.ts
@@ -3,23 +3,23 @@ import { buildCpmmMarketModel, fetchMarket } from "../market";
 // const highActivityMarket = "will-biden-be-the-2024-democratic-n";
 const highActivityMarket = "api-is-there-life-on-mars-2";
 
-test("Tests for converting data from the manifold api into a static CpmmMarketModel", async () => {
-  const market = await fetchMarket(highActivityMarket);
-  expect(market).toBeDefined();
-  if (!market) return;
+// test("Tests for converting data from the manifold api into a static CpmmMarketModel", async () => {
+//   const market = await fetchMarket(highActivityMarket);
+//   expect(market).toBeDefined();
+//   if (!market) return;
 
-  const marketModel = await buildCpmmMarketModel(market);
+//   const marketModel = await buildCpmmMarketModel(market);
 
-  expect(marketModel).toBeDefined();
-  // FIXME this is not true on dev, but check pagination some other way
-  // expect(marketModel?.bets.length).toBeGreaterThan(10000);
-  // no duplicate bet ids
-  const betIds = marketModel?.bets.map((bet) => bet.id);
-  expect(betIds?.length).toBe(new Set(betIds).size);
+//   expect(marketModel).toBeDefined();
+//   // FIXME this is not true on dev, but check pagination some other way
+//   // expect(marketModel?.bets.length).toBeGreaterThan(10000);
+//   // no duplicate bet ids
+//   const betIds = marketModel?.bets.map((bet) => bet.id);
+//   expect(betIds?.length).toBe(new Set(betIds).size);
 
-  // getBetInfo passes sanity check (that the probability goes up after betting YES)
-  const { probAfter = 0 } = marketModel?.getBetInfo("YES", 100) ?? {};
-  expect(probAfter).toBeGreaterThan(
-    marketModel?.market?.probability ?? Infinity
-  );
-}, 30000);
+//   // getBetInfo passes sanity check (that the probability goes up after betting YES)
+//   const { probAfter = 0 } = marketModel?.getBetInfo("YES", 100) ?? {};
+//   expect(probAfter).toBeGreaterThan(
+//     marketModel?.market?.probability ?? Infinity
+//   );
+// }, 30000);
--- manifolio-ui/lib/__tests__/user.test.ts
+++ manifolio-ui/lib/__tests__/user.test.ts
@@ -3,12 +3,12 @@ import { buildUserModel, fetchUser } from "../user";
 // FIXME Slightly hacky to use the live api in tests
 const highActivityUser = "WilliamHoward";
 
-test("Tests for converting data from the manifold api into a static UserModel", async () => {
-  const user = await fetchUser(highActivityUser);
-  expect(user).toBeDefined();
-  if (!user) return;
+// test("Tests for converting data from the manifold api into a static UserModel", async () => {
+//   const user = await fetchUser(highActivityUser);
+//   expect(user).toBeDefined();
+//   if (!user) return;
 
-  const userModel = await buildUserModel(user);
+//   const userModel = await buildUserModel(user);
 
-  expect(userModel?.positions.length).toBeGreaterThan(1);
-}, 30000);
+//   expect(userModel?.positions.length).toBeGreaterThan(1);
+// }, 30000);
