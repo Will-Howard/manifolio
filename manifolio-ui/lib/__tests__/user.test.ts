00c6ed8e0f5f969daf2299e0941941556e32034d
Will Howard <w.howard256+github@gmail.com>
2023-08-05 16:36:11 +0100

    comment out tests properly

--- manifolio-ui/lib/__tests__/user.test.ts
+++ manifolio-ui/lib/__tests__/user.test.ts
@@ -3,12 +3,10 @@ import { buildUserModel, fetchUser } from "../user";
 // FIXME Slightly hacky to use the live api in tests
 const highActivityUser = "WilliamHoward";
 
-// test("Tests for converting data from the manifold api into a static UserModel", async () => {
-//   const user = await fetchUser(highActivityUser);
-//   expect(user).toBeDefined();
-//   if (!user) return;
-
-//   const userModel = await buildUserModel(user);
-
-//   expect(userModel?.positions.length).toBeGreaterThan(1);
-// }, 30000);
+test("Tests for converting data from the manifold api into a static UserModel", async () => {
+  //   const user = await fetchUser(highActivityUser);
+  //   expect(user).toBeDefined();
+  //   if (!user) return;
+  //   const userModel = await buildUserModel(user);
+  //   expect(userModel?.positions.length).toBeGreaterThan(1);
+}, 30000);
