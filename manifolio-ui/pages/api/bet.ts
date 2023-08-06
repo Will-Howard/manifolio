2e95c08c2212202073d0c3de839dfc8e7a32968f
Will Howard <w.howard256+github@gmail.com>
2023-08-06 16:17:27 +0100

    log metric when bet is placed

--- manifolio-ui/pages/api/bet.ts
+++ manifolio-ui/pages/api/bet.ts
@@ -1,6 +1,46 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Manifold } from "@/lib/vendor/manifold-sdk";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { client, v2 } from "@datadog/datadog-api-client";
+
+const configuration = client.createConfiguration();
+const apiInstance = new v2.MetricsApi(configuration);
+
+const submitDatadogMetric = async (
+  amount: number,
+  username: string,
+  success: boolean
+) => {
+  const params: v2.MetricsApiSubmitMetricsRequest = {
+    body: {
+      series: [
+        {
+          metric: "manifolio.bet",
+          type: 1, // count
+          points: [
+            {
+              timestamp: Math.round(new Date().getTime() / 1000),
+              value: amount,
+            },
+          ],
+          resources: [
+            {
+              name: "vercel",
+              type: "host",
+            },
+          ],
+          tags: [
+            `success:${success}`,
+            `username:${username}`,
+            `env:${process.env.NEXT_PUBLIC_ENV || "dev"}`,
+          ],
+        },
+      ],
+    },
+  };
+
+  await apiInstance.submitMetrics(params);
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,14 +48,21 @@ export default async function handler(
     betId: string;
   }>
 ) {
-  const { amount, outcome, marketId, apiKey } = req.body;
+  const { amount, outcome, marketId, apiKey, username } = req.body;
   const api = new Manifold(apiKey);
 
-  const createResult = await api.createBet({
-    amount: Math.round(amount),
-    marketId,
-    outcome,
-  });
+  try {
+    const createResult = await api.createBet({
+      amount: Math.round(amount),
+      marketId,
+      outcome,
+    });
 
-  res.status(200).json(createResult);
+    await submitDatadogMetric(amount, username, true);
+
+    res.status(200).json(createResult);
+  } catch (e) {
+    await submitDatadogMetric(amount, username, false);
+    res.status(500).json({ betId: "" });
+  }
 }
